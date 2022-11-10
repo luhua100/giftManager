8cd6b300f4171e09d254fe05017bc2ed78cd0ee2
luhua <luhua2245@163.com>
2022-11-10 10:41:59 +0800

    Update LHGiftModel.swift

--- braGiftManager/Classes/LHGiftModel.swift
+++ braGiftManager/Classes/LHGiftModel.swift
@@ -7,13 +7,13 @@
 
 import UIKit
 
-class LHGiftModel: NSObject {
+public class LHGiftModel: NSObject {
 
-    var picPath : String?
-    var id : String?
-    var giftName : String?
-    var sendCount : Int = 1 //礼物发送个数
-    var defaultCount : Int = 0 //默认个数
+   public var picPath : String?
+   public var id : String?
+   public var giftName : String?
+   public  var sendCount : Int = 1 //礼物发送个数
+   public  var defaultCount : Int = 0 //默认个数
     var giftKey : String?{
         get{
             return  "\(id ?? "")\(giftName ?? "")"
